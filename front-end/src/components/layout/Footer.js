d1da926981603f578924df8c69cfc7a2bd007649
seiyoon <sera4084@naver.com>
2023-04-18 10:38:31 +0900

    feat: footer hover

--- front-end/src/components/layout/Footer.js
+++ front-end/src/components/layout/Footer.js
@@ -9,25 +9,15 @@ export const Footer = () => {
       <div className="footerHome">
         <NavLink to={"/home"} className="footerHomeBtn">
           <Icon icon="material-symbols:home" className="footerHomeIcon" />
-        </NavLink>
-        <Link
-          to={"/home"}
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-        >
           <p>홈</p>
-        </Link>
+        </NavLink>
       </div>
 
       <div className="footerCourse">
         <NavLink to={"/course"} className="footerCourseBtn">
           <Icon icon="mdi:image-filter-hdr" className="footerCourseIcon" />
-        </NavLink>
-        <Link
-          to={"/course"}
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-        >
           <p>코스 보기</p>
-        </Link>
+        </NavLink>
       </div>
 
       <div className="footerRecord">
@@ -36,13 +26,8 @@ export const Footer = () => {
             icon="material-symbols:directions-run"
             className="footerRecordIcon"
           />
-        </NavLink>
-        <Link
-          to={"/record"}
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-        >
           <p>기록하기</p>
-        </Link>
+        </NavLink>
       </div>
 
       <div className="footerPlog">
@@ -51,25 +36,15 @@ export const Footer = () => {
             icon="material-symbols:calendar-view-month-sharp"
             className="footerPlogIcon"
           />
-        </NavLink>
-        <Link
-          to={"/plog"}
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-        >
           <p>플로그</p>
-        </Link>
+        </NavLink>
       </div>
 
       <div className="footerMy">
         <NavLink to={"/my"} className="footerMyBtn">
           <Icon icon="mdi:account" className="footerMyIcon" />
-        </NavLink>
-        <Link
-          to={"/my"}
-          className={({ isActive }) => (isActive ? "active" : undefined)}
-        >
           <p>마이페이지</p>
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
