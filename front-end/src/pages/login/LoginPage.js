5cf0fe5a512646bd08b7481658e70317796d50a8
seiyoon <sera4084@naver.com>
2023-05-04 12:20:19 +0900

    feat: 타이핑 효과 추가

--- front-end/src/pages/login/LoginPage.js
+++ front-end/src/pages/login/LoginPage.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Button } from "../../components/common/Button";
 import { ReactComponent as Logo } from "../../assets/icons/logo.svg";
 
@@ -5,6 +6,20 @@ import styled from "styled-components";
 import { COLOR } from "../../styles/color";
 
 function LoginPage() {
+  const [contentIndex, setContentIndex] = useState(0);
+  const content = "아름다운 서울을 위해\n우리 함께 주워봐요!";
+
+  // typing 함수
+  const typing = () => {
+    setContentIndex((index) => index + 1);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(typing, 200);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   const kakaoLogin = () => {
     window.location.href = "http://3.37.14.183/api/auth/login";
   };
@@ -13,15 +28,18 @@ function LoginPage() {
     <StLoginPage>
       <LoginText>
         <Logo />
-        <h6>
-          아름다운 서울을 위해
-          <br />
-          우리 함께 주워봐요!
-        </h6>
+        <p
+          className="txt"
+          dangerouslySetInnerHTML={{
+            __html: content.substring(0, contentIndex).replace(/\n/g, "<br>"),
+          }}
+        ></p>
       </LoginText>
-      <LoginButton>
-        <Button onClick={kakaoLogin}>카카오로 시작하기</Button>
-      </LoginButton>
+      {contentIndex > content.length && (
+        <LoginButton>
+          <Button onClick={kakaoLogin}>카카오로 시작하기</Button>
+        </LoginButton>
+      )}
     </StLoginPage>
   );
 }
@@ -38,14 +56,15 @@ const StLoginPage = styled.div`
 `;
 
 const LoginText = styled.div`
-  h6 {
+  p {
     margin-top: 36px;
     font-weight: 700;
     font-size: 20px;
-    line-height: 25px;
+    line-height: 32px;
     color: ${COLOR.MAIN_BLACK};
   }
 `;
+
 const LoginButton = styled.div`
   position: fixed;
   bottom: 0;
