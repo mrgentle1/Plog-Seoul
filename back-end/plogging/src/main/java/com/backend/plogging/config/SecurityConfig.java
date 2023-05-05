6c2aa5157b315e5c7f1259500a0a07352ad096f6
jun <59019322+mrgentle1@users.noreply.github.com>
2023-05-05 15:12:46 +0900

    fix #16 CORS 에러 해결

--- back-end/plogging/src/main/java/com/backend/plogging/config/SecurityConfig.java
+++ back-end/plogging/src/main/java/com/backend/plogging/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.backend.plogging.util.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -35,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
                 .antMatchers("/api/auth/login").permitAll()
                 .antMatchers("/api/auth/kakao").permitAll()
                 .antMatchers("/api/tests/**").permitAll()
