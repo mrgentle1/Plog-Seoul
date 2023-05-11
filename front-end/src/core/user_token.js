b0d666f05c32dd2982d278827a7520b88e4f5ba6
AnCha <tory912@gmail.com>
2023-05-11 11:38:03 +0900

    feat: trashCan api 연결 및 조건 추가

---
+++ front-end/src/core/user_token.js
@@ -0,0 +1,5 @@
+const user_token = {
+  token:
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiaWF0IjoxNjgzNzY0OTEwLCJleHAiOjE2ODM4MDA5MTB9.3OfgPW8OADv4xT9KgUdC_ljIj7ePqIHEpJdtmDKSWsk",
+};
+export { user_token };
