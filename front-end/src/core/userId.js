ddb462f99f00961c93d9ef8132abd338c906b607
seiyoon <sera4084@naver.com>
2023-05-16 19:32:12 +0900

    feat: userId recoil로 저장

---
+++ front-end/src/core/userId.js
@@ -0,0 +1,25 @@
+import { atom, useRecoilState } from "recoil";
+import { useEffect } from "react";
+
+export const userIdNumber = atom({
+  key: "userIdNumber",
+  default: null,
+});
+
+// recoil 값을 localStorage에 저장하고 복원하는 커스텀 훅
+export const usePersistRecoilState = (atom) => {
+  const [state, setState] = useRecoilState(atom);
+
+  useEffect(() => {
+    const savedState = localStorage.getItem(atom.key);
+    if (savedState !== null) {
+      setState(JSON.parse(savedState));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(atom.key, JSON.stringify(state));
+  }, [state]);
+
+  return [state, setState];
+};
