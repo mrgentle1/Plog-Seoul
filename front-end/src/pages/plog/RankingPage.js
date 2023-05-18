fb5ef5845fbf6e2cec552da0ea6cfadaa81290b9
AnCha <tory912@gmail.com>
2023-05-19 01:05:21 +0900

    init

---
+++ front-end/src/pages/plog/RankingPage.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useRef } from "react";
+import styled from "styled-components";
+import { COLOR } from "../../styles/color";
+import axios from "axios";
+
+function RankingPage() {
+  return <StRankingPage></StRankingPage>;
+}
+export default RankingPage;
+
+const StRankingPage = styled.div``;
