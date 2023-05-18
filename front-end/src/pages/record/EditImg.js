35571c5bc84633cf56c9fa788203006e8c6a9123
AnCha <tory912@gmail.com>
2023-05-19 00:57:01 +0900

    style: 사용하지 않는 부분 정리

--- front-end/src/pages/record/EditImg.js
+++ front-end/src/pages/record/EditImg.js
@@ -262,77 +262,7 @@ const SelectImg = styled.div`
   background-image: url(${(props) => props.image});
 `;
 
-const ModalText = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
-  margin-top: 27px;
-
-  width: 168px;
-  height: 200px;
-
-  .reviewCheck {
-    width: 36px;
-    height: 36px;
-  }
-  h3 {
-    margin-top: 28px;
-    font-weight: 700;
-    font-size: 17px;
-    line-height: 21px;
-    text-align: center;
-    color: ${COLOR.MAIN_BLACK};
-  }
-  h5 {
-    margin-top: 12px;
-    margin-bottom: 70px;
-    font-weight: 500;
-    font-size: 15px;
-    line-height: 19px;
-    text-align: center;
-    color: ${COLOR.INPUT_BORDER_GRAY};
-  }
-`;
-const ModalLine = styled.div`
-  width: 300px;
-  border: 0.25px solid ${COLOR.MAIN_GREEN_HOVER};
-`;
 const ModalButton = styled.div`
   display: flex;
   flex-direction: row;
-`;
-const CloseButton = styled.button`
-  width: 150px;
-  height: 48px;
-  color: ${COLOR.MAIN_GREEN};
-  background: ${COLOR.MAIN_WHITE};
-  border-radius: 0px 0px 0px 14px;
-  border: none;
-  font-weight: 500;
-  font-size: 15px;
-  line-height: 19px;
-`;
-const CheckButton = styled.button`
-  width: 150px;
-  height: 48px;
-  background: ${COLOR.MAIN_GREEN};
-  border-radius: 0px 0px 14px 0px;
-  border: none;
-  font-weight: 600;
-  font-size: 15px;
-  line-height: 19px;
-`;
-
-const FilterContainer = styled.div`
-  display: flex;
-  z-index: 2009;
-  position: relative;
-  background-image;
-  
-  .FilterImg {
-    display: flex;
-    position: relative;
-  }
 `;
