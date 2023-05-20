2b0048fcae3813a9aa33ccd24883a777a191a211
jun <59019322+mrgentle1@users.noreply.github.com>
2023-05-20 20:06:56 +0900

    feat: android 위치 권한 추가

--- App/app/src/main/java/com/plogseoul/application/WebViewActivity.java
+++ App/app/src/main/java/com/plogseoul/application/WebViewActivity.java
@@ -1,13 +1,18 @@
 package com.plogseoul.application;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.webkit.DownloadListener;
+import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class WebViewActivity extends AppCompatActivity {
     private String TAG = WebViewActivity.class.getSimpleName();
@@ -15,23 +20,43 @@ public class WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1234;
     private String rootUrl = "https://plog-seoul-git-develop-mrgentle1.vercel.app/";
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
+        // Check and request permissions
+        checkPermissions();
+
         webView = (WebView) findViewById(R.id.webview);
 
         webView.setWebViewClient(new WebViewClient());  // 새 창 띄우기 않기
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
 
         webView.getSettings().setLoadWithOverviewMode(true);  // WebView 화면크기에 맞추도록 설정 - setUseWideViewPort 와 같이 써야함
         webView.getSettings().setUseWideViewPort(true);  // wide viewport 설정 - setLoadWithOverviewMode 와 같이 써야함
 
         webView.getSettings().setSupportZoom(false);  // 줌 설정 여부
         webView.getSettings().setBuiltInZoomControls(false);  // 줌 확대/축소 버튼 여부
+
+        webView.getSettings().setGeolocationEnabled(true);  // GPS 사용 여부
 
         webView.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용여부
 //        webview.addJavascriptInterface(new AndroidBridge(), "android");
@@ -58,6 +83,22 @@ public class WebViewActivity extends AppCompatActivity {
         } else {
             backBtnTime = curTime;
             Toast.makeText(this, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한이 허용된 경우 실행할 코드
+                } else {
+                    // 권한이 거부된 경우 실행할 코드
+                }
+                return;
+            }
+            // 다른 'case' 줄을 이곳에 추가하여 다른 권한 요청을 확인할 수 있습니다.
         }
     }
 
