596ed07bfd10b2eab3d9f8cc9081df8ac5f19e73
AnCha <tory912@gmail.com>
2023-05-22 17:43:25 +0900

    test: 이미지 및 경로 확인

--- front-end/src/pages/record/RecordIng.js
+++ front-end/src/pages/record/RecordIng.js
@@ -232,7 +232,7 @@ function RecordIngPage() {
   const [imgData, setImgData] = useState([]);
   async function postImgData() {
     // async, await을 사용하는 경우
-    console.log("post직전 imgUrl확인:", imageUrl);
+    // console.log("post직전 imgUrl확인:", imageUrl);
     try {
       // GET 요청은 params에 실어 보냄
       const response = await axios.post(
@@ -260,6 +260,7 @@ function RecordIngPage() {
 
   async function postPathData() {
     // async, await을 사용하는 경우
+
     try {
       // GET 요청은 params에 실어 보냄
       const response = await axios.post(
@@ -460,6 +461,7 @@ function RecordIngPage() {
         setWatchId(-1);
 
         setRecording(false);
+        console.log("post직전 path확인:", locationList);
         patchRecordData();
         postPathData();
         navigate("/record/point", {
