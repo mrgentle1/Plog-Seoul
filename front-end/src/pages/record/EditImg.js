36fb07ad885c0e34d7f6ad669215a06c1223b4d5
jun <59019322+mrgentle1@users.noreply.github.com>
2023-05-29 15:08:12 +0900

    test: cors test

--- front-end/src/pages/record/EditImg.js
+++ front-end/src/pages/record/EditImg.js
@@ -107,7 +107,7 @@ export const EditImgModal = ({ setImgEditOpen, img, data }) => {
       imageTimeout: 15000, //newline
       scale: 3, //newline
       allowTaint: true,
-      useCORS: false,
+      useCORS: true,
     }).then((canvas) => {
       // setFilterImg(canvas.toDataURL("image/png"));
       postImgData(canvas.toDataURL("image/png"));
